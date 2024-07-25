04e9d6f9140e76da625ac09aafae29d28c3514f1
adityrajtiwary <122718170+adityrajtiwary@users.noreply.github.com>
2024-07-25 10:27:51 +0530

    Add files via upload

---
+++ Lecture 8 Cipher School.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+//	int a[4];
+//	cout<<"Give five integer as input"<<endl;
+//	
+//	for(int i=0;i<5;i++)
+//	{
+//		cin>>a[i];
+//	}
+//	cout<<"The 5 given inputs are: "<<endl;
+//	for(int i=0;i<5;i++)
+//	{
+//		cout<<a[i]<<" ";
+//	}
+
+//	int a[9];
+//	
+//	cout<<"Give 10 integer as input"<<endl;
+//	
+//	for(int i=0;i<10;i++)
+//	{
+//		cin>>a[i];
+//	}
+//	
+//	cout<<"The 10 give inputs are: "<<endl;
+//	for(int i=9;i>=0;i--)
+//	{
+//		cout<<a[i]<<endl;
+//	}
+	
+	return 0;
+	
+}
